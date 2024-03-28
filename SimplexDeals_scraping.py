68579bdc2d5bb82f0df52c9cfb99e353c5e57816
JunaidRafique69 <133365751+JunaidRafique69@users.noreply.github.com>
2024-03-28 21:41:39 +0500

    Update SimplexDeals_scraping.py

--- SimplexDeals_scraping.py
+++ SimplexDeals_scraping.py
@@ -11,11 +11,12 @@ driver = webdriver.Chrome()
 product_urls = []
 
 try:
-    page = 0
-    while True:
+    pages = [x for x in range(0, 1000)]
+    for page in pages:
         url = f'https://simplexdeals.com/collections/all-products?page={page}'
         driver.get(url)
 
+        # Wait for products to load
         WebDriverWait(driver, 10).until(
             EC.presence_of_all_elements_located((By.XPATH, "//div[@class='product grid__item medium-up--one-third "
                                                            "col2 small--one-half slide-up-animation animated']"))
@@ -41,27 +42,21 @@ try:
 
             except NoSuchElementException:
                 print("Element not found while processing product, skipping.")
-                continue
+                continue  # Continue to the next product
             except StaleElementReferenceException:
                 print("Stale element reference encountered, retrying.")
-                continue
+                continue  # Continue to the next product
             except Exception as e:
                 print(f"An unexpected error occurred for product: {str(e)}")
-                continue
-
-        page += 1
+                continue  # Continue to the next product
+        with open('product_urls_.csv', 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(['Product URL'])
+            writer.writerows([[url] for url in product_urls])
+        print('Product URLs stored in "product_urls.csv"')
 
 except Exception as ex:
     print(f"An error occurred: {str(ex)}")
 
 finally:
     driver.quit()
-
-if product_urls:
-    with open('product_urls_.csv', 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(['Product URL'])
-        writer.writerows([[url] for url in product_urls])
-    print('Product URLs stored in "product_urls.csv"')
-else:
-    print('No product URLs found.')
