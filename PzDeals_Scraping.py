8add3b1191497542fea7ae60337cbf8739eaadf9
JunaidRafique69 <133365751+JunaidRafique69@users.noreply.github.com>
2024-03-28 21:42:23 +0500

    Update PzDeals_Scraping.py

--- PzDeals_Scraping.py
+++ PzDeals_Scraping.py
@@ -13,17 +13,15 @@ product_urls = []
 try:
     pages = [x for x in range(0, 1000)]
     for page in pages:
-        url = f'https://simplexdeals.com/collections/all-products?page={page}'
+        url = f'https://www.pzdeals.com/?page={page}'
         driver.get(url)
 
         # Wait for products to load
         WebDriverWait(driver, 10).until(
-            EC.presence_of_all_elements_located((By.XPATH, "//div[@class='product grid__item medium-up--one-third "
-                                                           "col2 small--one-half slide-up-animation animated']"))
+            EC.presence_of_all_elements_located((By.XPATH, "//li[@class=' on-sale text-center']"))
         )
 
-        products = driver.find_elements(By.XPATH, "//div[@class='product grid__item medium-up--one-third col2 "
-                                                  "small--one-half slide-up-animation animated']")
+        products = driver.find_elements(By.XPATH, "//li[@class=' sold-out on-sale text-center']")
 
         if len(products) == 0:
             print(f"No more products on page {page}, exiting loop.")
@@ -33,7 +31,7 @@ try:
             try:
                 product.click()
                 product_link = WebDriverWait(driver, 10).until(
-                    EC.element_to_be_clickable((By.XPATH, "//div[@class='product-single__cart-submit-wrapper']//a"))
+                    EC.element_to_be_clickable((By.XPATH, "//div[@class='is_desktop']//a"))
                 )
                 url = product_link.get_attribute('href')
                 print(url)
